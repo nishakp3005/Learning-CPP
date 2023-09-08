86db79a7aa40ef7ca6d7b6f1a60466ec4691a9eb
Nishita <nishakp3005@gmail.com>
2023-09-08 16:27:12 +0530

    new pattern files added

--- pattern13.cpp
+++ pattern13.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main(){
     int n;
-    char c;
     cin>>n;
-    for (int  i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j<=i-1 ; j++)
+        for (int j = 0; j < n; j++)
         {
-            c = char(65+n-i+j);
-            cout<<c<<" ";
+            if(i>j) cout<<"  ";
+            else cout<<i+1<<" ";
         }
         cout<<endl;
     }
+    
     return 0;
 }
