bb67e1300a96be847c3564af557a07c361b85a64
Nishita <nishakp3005@gmail.com>
2023-09-16 12:56:46 +0530

    leet files added

--- 1009_leet.cpp
+++ 1009_leet.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main(){
-    
+    int n, mask = 0, ans = 0, temp;
+    cin>>n;
+    temp = n;
+    if(n == 0) return 1;
+    while(temp!=0){
+        mask = (mask << 1) | 1;
+        temp = temp >> 1; 
+    }
+    ans = (~n) & mask; 
+    cout<<mask<<endl<<ans;
     return 0;
 }
