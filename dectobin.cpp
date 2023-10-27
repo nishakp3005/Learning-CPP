08a5b2ba7e32a19a26ceba7b14f893fd24eb57f2
Nishita <nishakp3005@gmail.com>
2023-10-27 17:09:00 +0530

    some files updated

--- dectobin.cpp
+++ dectobin.cpp
@@ -2,14 +2,22 @@
 using namespace std;
 
 int main(){
-    int n ,rem, num = 0, i = 0;
-    cin>>n;
-    while(n!=0){
-        rem = n%2;
-        num = (pow(10, i) * rem) + num;
-        n = n/2;
-        i++;
+    int n;
+    cin >> n;
+    
+    if (n == 0) {
+        cout << 0; // Special case for n = 0
+    } else {
+        vector<int> binary;
+        
+        while (n > 0) {
+            binary.push_back(n % 2);
+            n = n / 2;
+        }
+        
+        for (int i = binary.size() - 1; i >= 0; i--) {
+            cout << binary[i];
+        }
     }
-    cout<<num;
     return 0;
 }
--- pattern2.cpp
+++ pattern2.cpp
@@ -2,10 +2,6 @@
 using namespace std;
  
 int main(){
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
     int a;
     cin>>a;
     for (int i = 1; i <= a; i++)
